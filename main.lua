028d37a1fb1b09bf028214ed5e358cd29133fa82
Dmitriy Balashov <mockedcat@gmail.com>
2019-02-12 12:21:42 +0300

    retro font was added

--- main.lua
+++ main.lua
@@ -12,14 +12,35 @@ function love.load()
   -- use nearest-neighbor filtering on upscaling and downscaling to prevent blurring of text 
   -- and graphics
   love.graphics.setDefaultFilter('nearest', 'nearest')
+
+  -- game fonts
+  local smallFont = love.graphics.newFont('font.ttf', 8)
+
+  love.graphics.setFont(smallFont)
   
-    push:setupScreen(gameWidth, gameHeight, windowWidth, windowHeight, {
+  push:setupScreen(gameWidth, gameHeight, windowWidth, windowHeight, {
     fullscreen = false,
     resizable = false,
     vsync = true
   })
 end
 
+--[[
+    Keyboard handling, called by LÖVE2D each frame; 
+    passes in the key we pressed so we can access.
+]]
+function love.keypressed(key)
+  -- keys can be accessed by string name
+  if key == 'escape' then
+      -- function LÖVE gives us to terminate application
+      love.event.quit()
+  end
+end
+
+--[[
+    Called after update by LÖVE2D, used to draw anything to the screen, 
+    updated or otherwise.
+]]
 function love.draw()
     -- begin rendering at virtual resolution
     push:apply('start')
